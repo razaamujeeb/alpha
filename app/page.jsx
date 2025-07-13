ddfc6b2701a00b4028e38329d11a6ad2cb11c7b9
razaamujeeb <razaamujeeb@gmail.com>
2025-07-13 23:12:47 +0900

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -12,46 +12,17 @@ export default function Page() {
   };
 
   const encodedQuery = encodeURIComponent(query);
- const mainSites = [
-  {
-    name: "楽天",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/8/8f/Rakuten_Global_Brand_Logo.svg",
-    baseUrl: "https://search.rakuten.co.jp/search/mall/",
-  },
-  {
-    name: "ヤフオク",
-    logo: "https://auctions.yahoo.co.jp/favicon.ico",
-    baseUrl: "https://auctions.yahoo.co.jp/search/search?p=",
-  },
-  {
-    name: "メルカリ",
-    logo: "https://static.mercdn.net/images/logo/mercari_logo.png",
-    baseUrl: "https://www.mercari.com/jp/search/?keyword=",
-  },
-  {
-    name: "ジモティー",
-    logo: "https://jmty.jp/favicon.ico",
-    baseUrl: "https://jmty.jp/all/sale?keyword=",
-    }
-  {
-    name: "Amazon",
-    logo: "https://www.amazon.co.jp/favicon.ico",
-    baseUrl: "https://www.amazon.co.jp/s?k=",
-  }
+  const mainSites = [
+    { name: "楽天", url: `https://search.rakuten.co.jp/search/mall/${encodedQuery}/`, img: "https://upload.wikimedia.org/wikipedia/commons/8/8f/Rakuten_Global_Brand_Logo.svg" },
+    { name: "ヤフオク", url: `https://auctions.yahoo.co.jp/search/search?p=${encodedQuery}`, img: "https://auctions.yahoo.co.jp/favicon.ico" },
+    { name: "メルカリ", url: `https://jp.mercari.com/search?keyword=${encodedQuery}`, img: "https://static.mercdn.net/images/logo/mercari_logo.png" },
+    { name: "ジモティー", url: `https://jmty.jp/all/sale?keyword=${encodedQuery}`, img: "https://jmty.jp/favicon.ico" },
+    { name: "Amazon", url: `https://www.amazon.co.jp/s?k=${encodedQuery}`, img: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" }
   ];
-
   const manualSites = [
-  {
-    name: "モノタロウ",
-    logo: "https://www.monotaro.com/favicon.ico",
-    baseUrl: "https://www.monotaro.com/s/?keyword=",
-  },
-  {
-    name: "カーセンサー",
-    logo: "https://www.carsensor.net/favicon.ico",
-    baseUrl: "https://www.carsensor.net/usedcar/search.php?KEYWORD=",
-  },
-];
+    { name: "モノタロウ", url: "https://www.monotaro.com/", img: "https://www.monotaro.com/favicon.ico" },
+    { name: "カーセンサー", url: "https://www.carsensor.net/", img: "https://www.carsensor.net/favicon.ico" }
+  ];
 
   return (
     <main style={{ fontFamily: "Arial, sans-serif", textAlign: "center", paddingTop: "4rem" }}>
@@ -88,20 +59,32 @@ export default function Page() {
       </form>
 
       {submitted && (
-        <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", gap: "2rem", marginTop: "3rem" }}>
-          {sites.map((site) => (
-            <a
-              key={site.name}
-              href={site.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ textAlign: "center", width: "120px" }}
-            >
-              <img src={site.img} alt={site.name} style={{ width: "100%", maxHeight: "80px", objectFit: "contain" }} />
-              <div style={{ marginTop: "0.5rem", color: "#333", fontSize: "0.9rem" }}>{site.name}</div>
-            </a>
-          ))}
-        </div>
+        <>
+          <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", gap: "2rem", marginTop: "3rem" }}>
+            {mainSites.map((site, index) => (
+              <div key={index} style={{ textAlign: "center", width: "120px" }}>
+                <a href={site.url} target="_blank" rel="noopener noreferrer">
+                  <img src={site.img} alt={site.name} style={{ width: "100%", maxHeight: "80px", objectFit: "contain" }} />
+                </a>
+                <div style={{ marginTop: "0.5rem", color: "#333", fontSize: "0.9rem" }}>{site.name}</div>
+              </div>
+            ))}
+          </div>
+
+          <div style={{ marginTop: "5rem", textAlign: "center" }}>
+            <p style={{ fontWeight: "bold", fontSize: "1rem", marginBottom: "1rem" }}>以下手動</p>
+            <div style={{ display: "flex", justifyContent: "center", gap: "2rem" }}>
+              {manualSites.map((site, index) => (
+                <div key={index} style={{ textAlign: "center", width: "100px" }}>
+                  <a href={site.url} target="_blank" rel="noopener noreferrer">
+                    <img src={site.img} alt={site.name} style={{ width: "100%", maxHeight: "60px", objectFit: "contain" }} />
+                  </a>
+                  <div style={{ marginTop: "0.3rem", color: "#555", fontSize: "0.8rem" }}>{site.name}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
       )}
     </main>
   );
